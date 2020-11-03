ffd4cc60f0364f87eb7623472e63a5ed3e40a45a
EricMTodd <eterictodd@gmail.com>
2020-11-03 12:42:17 -0700

    Basic buttons added and will update the document.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # calculator
 
 Lesson link: https://theodinproject.com/lessons/calculator
+
+# TODO:
+
+- Consider how the display is writing to the document. Try using strings instead of an array, to evaluate the display.
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,71 @@
 const calculator = {
-  display: [1, 2, 3, 4],
-  sum: function(display) {
-    let sum = 0;
-    for (let i = 0; i < display.length; i++) {
-      sum += display[i];
-    };
-    console.log(sum);
-  },
-};
+  // Display array
+  display: [],
 
-let display = calculator.display;
-const sum = function(display) {
-  calculator.sum(display);
+  // Operations
+  sum: function() {
+    console.log("SUM");
+  },
+  difference: function() {
+    console.log("DIFFERENCE");
+  },
+  product: function() {
+    console.log("PRODUCT");
+  },
+  quotient: function() {
+    console.log("QUOTIENT");
+  },
+  evaluate: function() {
+    console.log("EVALUATE");
+  },
+
+  // Values
+  one: function() {
+    this.display.push(1)
+    this.updateDisplay();
+  },
+  two: function() {
+    this.display.push(2)
+    this.updateDisplay();
+  },
+  three: function() {
+    this.display.push(3)
+    this.updateDisplay();
+  },
+  four: function() {
+    this.display.push(4)
+    this.updateDisplay();
+  },
+  five: function() {
+    this.display.push(5)
+    this.updateDisplay();
+  },
+  six: function() {
+    this.display.push(6)
+    this.updateDisplay();
+  },
+  seven: function() {
+    this.display.push(7)
+    this.updateDisplay();
+  },
+  eight: function() {
+    this.display.push(8)
+    this.updateDisplay();
+  },
+  nine: function() {
+    this.display.push(9)
+    this.updateDisplay();
+  },
+  zero: function() {
+    this.display.push(0)
+    this.updateDisplay();
+  },
+  decimal: function() {
+    console.log(".")
+  },
+
+  // Other methods
+  updateDisplay: function() {
+    document.getElementById("display").innerHTML = this.display;
+  }
 };
