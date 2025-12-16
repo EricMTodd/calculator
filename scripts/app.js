c4e4e8c7d4eb6f927a06dbbc3c4ab9e7cdbf6925
EricMTodd <eterictodd@gmail.com>
2025-12-16 13:14:29 -0700

    Event listeners assigned. Working on evaluations.

--- scripts/app.js
+++ scripts/app.js
@@ -1,121 +1,23 @@
 // Global constants
-const display = document.querySelector(`.display`)
-const buttons = document.querySelectorAll(`.button`)
+const display = document.querySelector('.display')
+const buttons = Array.from(document.querySelectorAll('.button'))
 
 
-// Global variables
-let currentExpression = ""
-let evaluatedExpression = ""
-
-
-// Function declarations
-const pressExpressionButton = (event) => {
-	const n = event.target.textContent
-	currentExpression += n
-	display.textContent = currentExpression
-}
-
-const pressClearButton = (event) => {
-	currentExpression = ""	
-	display.textContent = currentExpression
-}
-
-const pressDeleteButton = (event) => {
-	currentExpression = currentExpression.slice(0, -1)
-	display.textContent = currentExpression
-}
-
-evaluatePostFix = (rpnQueue) => {
-	const operandStack = []
-
-	rpnQueue.forEach(item => {
-		if(!isNaN(parseFloat(item)) && isFinite(item) && item !== "") {
-			operandStack.push(parseFloat(item))
-		} else {
-			const operandB = operandStack.pop()
-			const operandA = operandStack.pop()
-			let result = 0
-			if (item === "+") {
-				result = operandA + operandB
-			}
-			if (item === "-") {
-				result = operandA - operandB
-			}
-			if (item === "×") {
-				result = operandA * operandB
-			}
-			if (item === "÷") {
-				result = operandA / operandB
-			}
-			operandStack.push(result)
-		}
-	})
-	return operandStack.pop()
-}
-
-const evaluateExpression = (event) => {
-	// Shunting Yard Algorithm
-	n = ""
-	const queue = []
-	const stack = []
-
-	Array.from(currentExpression).forEach(item => {
-		if (Number.isInteger(Number(item))) {
-			n += item
-		} else {
-			queue.push(n)
-			n = ""
-			if (item === "(") {
-				stack.push(item)
-			} else if (item === ")") {
-				while (stack.length > 0 && stack[stack.length - 1] !== "(") {
-					queue.push(stack.pop())
-				}
-				stack.pop()
-			} else {
-				if (item === "×" || item === "÷") {
-					while (stack.length > 0 && stack[stack.length - 1] !== "(" && (stack[stack.length -1] === "×" || stack[stack.length - 1] === "÷")) {
-						queue.push(stack.pop())
-					}
-				} else if (item === "+" || item === "-") {
-					while (stack.length > 0 && stack[stack.length - 1] !== "(") {
-						let top = stack[stack.length - 1]
-						if (top === "×" || top === "÷" || top === "+" || top === "-") {
-							queue.push(stack.pop())
-						} else {
-							break
-						}
-					}
-				} 
-				stack.push(item)
-			}
-		}
-	})
-	queue.push(n)
-	n = ""
-	while (stack.length > 0) {
-		queue.push(stack.pop())
-	}
-
-	// Postfix evalution
-	const finalResult = evaluatePostFix(queue)
-	currentExpression = finalResult
-	display.textContent = currentExpression
-	return currentExpression
-}
-
-// Event listeners
+// Assign event listeners
 buttons.forEach(button => {
-	if (button.textContent !== "=" && button.textContent !== "Del" && button.textContent !== "C") {
-		button.addEventListener("click", pressExpressionButton)
-	}
-	if (button.textContent === "C") {
-		button.addEventListener("click", pressClearButton)
-	}
-	if (button.textContent === "Del") {
-		button.addEventListener("click", pressDeleteButton)
-	}
-	if (button.textContent === "=") {
-		button.addEventListener("click", evaluateExpression)
-	}
+	button.addEventListener('click', (e) => {
+		switch(e.target.textContent) {
+		case "C":
+			display.textContent = ""
+			break
+		case "Del":
+			display.textContent = display.textContent.slice(0, -1)
+			break
+		case "=":
+			console.log("Evaluate")
+			break
+		default:
+			display.textContent += e.target.innerText
+		}
+	})
 })
