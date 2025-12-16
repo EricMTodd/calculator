1a2dbc66f29c64d734e51a36192c2af43520e7ec
EricMTodd <eterictodd@gmail.com>
2025-12-16 15:36:34 -0700

    Working shuting yard algorithm implemented.

--- scripts/app.js
+++ scripts/app.js
@@ -9,12 +9,22 @@ const ReversePolishNotationEvaluation = () => {
 }
 
 const ShuntingYardAlgorithm = (expression) => {
+	expression = expression.split('')
+	for (let i = 0; i < expression.length; i++) {
+		console.log(expression[i])
+		if (expression[i] === '(') {
+			expression.splice(i, 0, '×')
+			i++
+		}
+	}
+	console.log(`Expression: ${expression}`)
+
 	let n = ''
 	let queue = []
 	let stack = []
 
-	Array.from(expression).forEach(token => {
-		
+	expression.forEach(token => {
+
 		if (!isNaN(parseFloat(token)) && isFinite(token) || token === '.') {
 			n += token
 		} else {
@@ -26,17 +36,38 @@ const ShuntingYardAlgorithm = (expression) => {
 			switch(token) {
 			case '(':
 				// Push the opening parenthesis directly to the stack.
+				stack.push(token)
 				break
 			case ')':
-				// Use a while loop to successively push -> pop: queue.push(stack.pop()) until reaching the opening parenthesis. Discard both parenthesis.
+				while (stack.length > 0 && stack[stack.length - 1] !== '(') {
+					// successively push -> pop: queue.push(stack.pop()) until reaching the opening parenthesis. Discard both parenthesis.
+					queue.push(stack.pop())
+				}
+				stack.pop()
 				break
 			case '+':
 			case '-':
-				// Check for the top element of the stack to see if it has greater or equal precedence. If it does, use a while loop to pop -> push: queue.push(stack.pop()) the elements until an opening parenthesis or a operator of lesser precedence is found.
+				// Check for the stack[stack.length - 1] element of the stack to see if it has greater or equal precedence. If it does, use a while loop to...
+				if (stack[stack.length - 1] === '+' || stack[stack.length - 1] === '-' || stack[stack.length - 1] === '×' || stack[stack.length - 1] === '÷') {
+					while (stack.length > 0 && stack[stack.length - 1] !== '(') {
+						// pop -> push: queue.push(stack.pop()) the elements until an opening parenthesis or a operator of lesser precedence is found.
+						queue.push(stack.pop())
+					}
+				} else {
+					stack.push(token)
+				}
 				break
 			case '×':
 			case '÷':
-				// Check for the top element of the stack to see if it has greater or equal precedence. If it does, use a while loop to pop -> push: queue.push(stack.pop()) the elements until an opening parenthesis or a operator of lesser precedence is found.
+				// Check for the stack[stack.length - 1] element of the stack to see if it has greater or equal precedence. If it does, use a while loop to...
+				if (stack[stack.length - 1] === '×' || stack[stack.length - 1] === '÷') {
+					while (stack.length > 0 && stack[stack.length - 1] !== '(') {
+						// pop -> push: queue.push(stack.pop()) the elements until an opening parenthesis or a operator of lesser precedence is found.
+						queue.push(stack.pop())
+					}
+				} else {
+					stack.push(token)
+				}
 				break
 			default:
 				n += token
@@ -49,10 +80,11 @@ const ShuntingYardAlgorithm = (expression) => {
 		n = ''
 	}
 
-	// While stack length > 0, queue.push(stack.pop())
+	while (stack.length > 0) {
+		queue.push(stack.pop())
+	}
 
 	console.log(`Queue: ${queue}`)
-	console.log(`Stack: ${stack}`)
 }
 
 
