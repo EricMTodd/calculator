333adc1dafdae186c674267c2d57040e7e7bea7c
EricMTodd <eterictodd@gmail.com>
2025-12-16 18:32:57 -0700

    Made some ASCII art for funsies.

--- scripts/app.js
+++ scripts/app.js
@@ -1,33 +1,25 @@
-// Global constants
+ // __   __        _      _    _        
+ // \ \ / /_ _ _ _(_)__ _| |__| |___ ___
+ //  \ V / _` | '_| / _` | '_ \ / -_|_-<
+ //   \_/\__,_|_| |_\__,_|_.__/_\___/__/                                  
 const display = document.querySelector('.display')
 const buttons = Array.from(document.querySelectorAll('.button'))
-
-
-// Function declarations
-const ReversePolishNotationEvaluation = () => {
-	// Some code
-}
-
-const ShuntingYardAlgorithm = (expression) => {
-	// Splits the expression string and inserts a multiplication symbol before any opening parenthesis
+ //  ___             _   _             
+ // | __|  _ _ _  __| |_(_)___ _ _  ___
+ // | _| || | ' \/ _|  _| / _ \ ' \(_-<
+ // |_| \_,_|_||_\__|\__|_\___/_||_/__/                                                                                                                     
+const shuntingYardAlgorithm = (expression) => {
 	expression = expression.split('')
 	for (let i = 0; i < expression.length; i++) {
-		if (expression[i] === '(') {
+		if (i !== 0 && (expression[i] == '(')) {
 			expression.splice(i, 0, '×')
 			i++
 		}
 	}
 
-	// Establish an accumulator for multidigit numbers
 	let n = ''
-
-	// Establish a queue for output
 	let queue = []
-
-	// Establish a stack for operators
 	let stack = []
-
-	// Iterates over each element of the expression array (to be continued)
 	expression.forEach(token => {
 		if (!isNaN(parseFloat(token)) && isFinite(token) || token === '.') {
 			n += token
@@ -78,15 +70,20 @@ const ShuntingYardAlgorithm = (expression) => {
 	while (stack.length > 0) {
 		queue.push(stack.pop())
 	}
-	console.log(`Queue: ${queue}`)
+	return queue
 }
 
-const evaluateExpression = () =>{
-	// some code executing the above functions in a single function.
+const reversePolishNotationEvaluation = (arr) => {
+	return arr
 }
 
-
-// Assign event listeners
+const evaluateExpression = (expression) =>{
+	console.log(reversePolishNotationEvaluation(shuntingYardAlgorithm(expression)))
+}
+ //  ___             _     _ _    _                       
+ // | __|_ _____ _ _| |_  | (_)__| |_ ___ _ _  ___ _ _ ___
+ // | _|\ V / -_) ' \  _| | | (_-<  _/ -_) ' \/ -_) '_(_-<
+ // |___|\_/\___|_||_\__| |_|_/__/\__\___|_||_\___|_| /__/                                              
 buttons.forEach(button => {
 	button.addEventListener('click', (e) => {
 		switch(e.target.textContent) {
@@ -97,10 +94,11 @@ buttons.forEach(button => {
 			display.textContent = display.textContent.slice(0, -1)
 			break
 		case "=":
-			ShuntingYardAlgorithm(display.textContent)
+			evaluateExpression(display.textContent)
 			break
 		default:
 			display.textContent += e.target.innerText
+			display.scrollLeft = display.scrollWidth
 		}
 	})
 })
